55febc28a032c17d525c03a0c7b020829e77b0d4
Nazickj2023 <158591875+Nazickj2023@users.noreply.github.com>
2025-09-29 13:17:44 +0300

    .

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,21 +1,33 @@
 using UnityEngine;
-using UnityEngine.Timeline;
 
 public class Enemy : MonoBehaviour
 {
+    public bool isAlive = true; // флаг жизни
+
     private bool isLeft = false;
     private bool isAttack = false;
     private Animator animator;
 
-    [SerializeField] Transform detectionZone; // точка проверки (перед врагом)
+    
+    
+
+    [SerializeField] Transform detectionZone;
     [SerializeField] Transform detectionZoneRight; 
-    [SerializeField] float radius = 5f;
-    [SerializeField] LayerMask hero;
+    [SerializeField] float radius = 10f;
+    [SerializeField] LayerMask hero ;
+   
     [SerializeField] Hero heroObject;
+
+    
+   
+    
+    
     private bool attacking = false;
 
     void Start()
     {
+        heroObject = FindObjectOfType<Hero>();// находим объект героя в сцене
+        hero = 1 << heroObject.gameObject.layer;// получаем слой героя
         animator = GetComponent<Animator>();
     }
 
@@ -27,22 +39,17 @@ public class Enemy : MonoBehaviour
     private void Flip()
     {
         Collider2D player = Physics2D.OverlapCircle(detectionZone.position, radius, hero);
-        Debug.Log(player);
         if (player != null)
         {
-            // если игрок слева
             if (player.transform.position.x < transform.position.x && !isLeft)
             {
                 transform.localScale = new Vector3(-1, 1, 1);
                 isLeft = true;
-                // heroObject.getDamage();
             }
-            // если игрок справа
             else if (player.transform.position.x > transform.position.x && isLeft)
             {
                 transform.localScale = new Vector3(1, 1, 1);
                 isLeft = false;
-                // Invoke("attackHero", 11100);
             }
             if (!attacking)
             {
@@ -51,30 +58,20 @@ public class Enemy : MonoBehaviour
             }
             isAttack = true;
             animator.SetBool("isAttack", isAttack);
-            // heroObject.getDamage(); 
         }
         else
         {
             animator.SetBool("isAttack", false);
         }
     }
+
     private void attackHero()
     {
         Collider2D player = Physics2D.OverlapCircle(detectionZoneRight.position, radius, hero);
-        // if (player.transform.position.x < transform.position.x && !isLeft)
-        // {
-        //     heroObject.getDamage();
-        // }
-        // else if (player.transform.position.x > transform.position.x && isLeft)
-        // {
-        //     heroObject.getDamage();
-        // }
         if (player)
         {
             heroObject.getDamage();
         }
         attacking = false;
     }
-
-
 }
