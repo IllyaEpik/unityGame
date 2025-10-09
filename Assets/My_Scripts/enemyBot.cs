dc63c5cb5e3c8e9e5e7343a334a0c78694bddab4
NikitaHuslystyi <nikitaguslistyj@gmail.com>
2025-10-09 21:35:02 +0300

    AiBot

--- Assets/My_Scripts/enemyBot.cs
+++ Assets/My_Scripts/enemyBot.cs
@@ -4,39 +4,63 @@ using UnityEngine;
 public class enemyBot : MonoBehaviour
 {
     private bool isLeft = false;
-
     Vector2 size = new Vector2(25, 3);
-    private bool attacking = false;
 
-    private int count = 200;
     private float cooldown = 2;
     private float cooldownCurrent = 0;
     private Animator animator;
+
     [SerializeField] Transform FirePoint;
     [SerializeField] Transform detectionZone;
     [SerializeField] Transform detectionZoneRight;
     [SerializeField] LayerMask hero;
     [SerializeField] Hero heroObject;
     [SerializeField] private GameObject plasmaPrefab;
-        private bool isAttack = false;
+
+    public float safeDistance = 3f;
+    public float moveSpeed = 2f;
+    public float jumpForce = 5f;
+    private Rigidbody2D rb;
+    private bool isGrounded = true;
+
     private void Start()
     {
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
-    {
-        
-    }
     private void FixedUpdate()
     {
-        // Flip();
-        // count--;
         cooldownCurrent -= Time.fixedDeltaTime;
         Flip();
-        // Collider2D[] left = Physics2D.OverlapBoxAll(detectionZone.position, size, 0f, hero);
-        // Debug.Log(count);
+        CheckPlayerDistance();
     }
+    public void OnPlayerShoot()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+    private void CheckPlayerDistance()
+    {
+        if (heroObject == null) return;
+
+        float distance = Vector2.Distance(transform.position, heroObject.transform.position);
+        if (distance < safeDistance)
+        {
+            Vector2 dir = (transform.position - heroObject.transform.position).normalized;
+            transform.position += (Vector3)(dir * moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+    }
+
     public void getDamageForBot()
     {
         Destroy(gameObject);
@@ -46,7 +70,7 @@ public class enemyBot : MonoBehaviour
     {
         Collider2D[] right = Physics2D.OverlapBoxAll(detectionZoneRight.position, size, 0f, hero);
         Collider2D[] left = Physics2D.OverlapBoxAll(detectionZone.position, size, 0f, hero);
-        // Debug.Log(left);
+
         if (right.Length != 0 || left.Length != 0)
         {
             foreach (Collider2D col in right)
@@ -61,20 +85,17 @@ public class enemyBot : MonoBehaviour
             {
                 if (col.CompareTag("hero"))
                 {
-                    // transform.localScale *= new Vector2(-1, 1);
                     animator.SetBool("isAttack", true);
                     attackHero(0);
                 }
             }
-            
-            
-                
         }
         else
         {
             animator.SetBool("isAttack", false);
         }
     }
+
     private void attackHero(int left)
     {
         if (cooldownCurrent <= 0)
@@ -83,14 +104,4 @@ public class enemyBot : MonoBehaviour
             cooldownCurrent = cooldown;
         }
     }
-    // private void OnTriggerStay2D(Collider2D collision)
-    // {
-    //     if (collision.CompareTag("hero") && count <= 0)
-    //     {
-    //         count = 200;
-    //         attackHero();
-    //     }
-    // }
-
-    
-}   
+}
